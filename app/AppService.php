74f885527c879dde21edb4249b32586dc0b3ef0a
疾风 <963606242@qq.com>
2021-10-18 17:13:51 +0800

    git push -u origin master

--- app/AppService.php
+++ app/AppService.php
@@ -1,10 +1,18 @@
 <?php
+/*
+ * @Author: your name
+ * @Date: 2021-10-08 10:56:23
+ * @LastEditTime: 2021-10-18 17:12:17
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \WWW\tp30\app\AppService.php
+ */
 declare (strict_types = 1);
 
 namespace app;
 
 use think\Service;
-
+//这里要一个备注
 /**
  * 应用服务类
  */
