82b19d2cf103ef44d86882f24a112f8bd98c6fa3
疾风 <963606242@qq.com>
2021-10-19 14:41:53 +0800

    分页类

--- app/provider.php
+++ app/provider.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * @Author: your name
+ * @Date: 2021-10-08 10:56:23
+ * @LastEditTime: 2021-10-19 14:39:14
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \tp30\app\provider.php
+ */
 use app\ExceptionHandle;
 use app\Request;
 
@@ -6,4 +14,5 @@ use app\Request;
 return [
     'think\Request'          => Request::class,
     'think\exception\Handle' => ExceptionHandle::class,
+    'think\Paginator'        =>'app\common\Bootstrap5'
 ];
